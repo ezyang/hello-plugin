835e54d6855f377c2d105e33a2fa928fac0517a6
Edward Z. Yang <ezyang@cs.stanford.edu>
2017-02-08 17:40:02 -0800

    Place -user-package-db at end of args to handle GHCi scripts.
    Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

--- HelloWrapper.hs
+++ HelloWrapper.hs
@@ -11,10 +11,5 @@ main = do
           "--interactive" ->
             ["--frontend", "Hello",
              "-plugin-package", "hello-plugin"]
-          -- If you used 'cabal install' to install hello-plugin,
-          -- this line is enough; but if you installed it in an
-          -- unusual package database, you will have to modify
-          -- this line to feed the correct database(s) in
-          "-no-user-package-db" | interactive -> []
           _ -> return arg
-  executeFile ghc False args' Nothing
+  executeFile ghc False (args' ++ if interactive then ["-user-package-db"] else []) Nothing
